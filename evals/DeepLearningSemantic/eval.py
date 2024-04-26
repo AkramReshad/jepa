0d283a5c1d26e7e5c06a8deb783eb02853976ffc
Akram200 <reshad@nostra.ai>
2024-04-26 15:14:14 -0400

    encoded videos

--- evals/DeepLearningSemantic/eval.py
+++ evals/DeepLearningSemantic/eval.py
@@ -21,6 +21,7 @@ import logging
 import pprint
 
 import numpy as np
+import os
 
 import torch
 import torch.multiprocessing as mp
@@ -28,25 +29,10 @@ import torchvision.transforms as transforms
 
 from torch.nn.parallel import DistributedDataParallel
 
-from timm.data import create_transform as timm_make_transforms
-
+from evals.video_classification_frozen.utils import (
+    ClipAggregation,
+)
 import src.models.vision_transformer as vit
-from src.models.attentive_pooler import AttentiveClassifier
-from src.datasets.data_manager import (
-    init_data,
-)
-from src.utils.distributed import (
-    init_distributed,
-    AllReduce
-)
-from src.utils.schedulers import (
-    WarmupCosineSchedule,
-    CosineWDSchedule,
-)
-from src.utils.logging import (
-    AverageMeter,
-    CSVLogger
-)
 
 logging.basicConfig()
 logger = logging.getLogger()
@@ -81,19 +67,15 @@ def main(args_eval, resume_preempt=False):
     pretrained_path = os.path.join(pretrain_folder, ckp_fname)
     # Optional [for Video model]:
     tubelet_size = args_pretrain.get('tubelet_size', 2)
-    frames_per_clip = args_pretrain.get('frames_per_clip', 1)
+    pretrain_frames_per_clip = args_pretrain.get('frames_per_clip', 1)
 
-    # -- DATA
-    args_data = args_eval.get('data')
-    dataset_name = args_data.get('dataset_name')
-    num_classes = args_data.get('num_classes')
-    root_path = args_data.get('root_path', None)
-    image_folder = args_data.get('image_folder', None)
-    resolution = args_data.get('resolution', 224)
+
 
     # -- OPTIMIZATION
     args_opt = args_eval.get('optimization')
+    resolution = args_opt.get('resolution', 224)
     batch_size = args_opt.get('batch_size')
+    attend_across_segments = args_opt.get('attend_across_segments', False)
     num_epochs = args_opt.get('num_epochs')
     wd = args_opt.get('weight_decay')
     start_lr = args_opt.get('start_lr')
@@ -102,9 +84,6 @@ def main(args_eval, resume_preempt=False):
     warmup = args_opt.get('warmup')
     use_bfloat16 = args_opt.get('use_bfloat16')
 
-    # -- EXPERIMENT-ID/TAG (optional)
-    resume_checkpoint = args_eval.get('resume_checkpoint', False) or resume_preempt
-    eval_tag = args_eval.get('tag', None)
 
     # ----------------------------------------------------------------------- #
 
@@ -119,34 +98,35 @@ def main(args_eval, resume_preempt=False):
         device = torch.device('cuda:0')
         torch.cuda.set_device(device)
 
-    world_size, rank = init_distributed()
-    logger.info(f'Initialized (rank/world-size) {rank}/{world_size}')
-
-    # -- log/checkpointing paths
-    folder = os.path.join(pretrain_folder, 'image_classification_frozen/')
-    if eval_tag is not None:
-        folder = os.path.join(folder, eval_tag)
-    if not os.path.exists(folder):
-        os.makedirs(folder, exist_ok=True)
-    log_file = os.path.join(folder, f'{tag}_r{rank}.csv')
-    latest_path = os.path.join(folder, f'{tag}-latest.pth.tar')
-
-    # -- make csv_logger
-    if rank == 0:
-        csv_logger = CSVLogger(log_file,
-                               ('%d', 'epoch'),
-                               ('%.5f', 'loss'),
-                               ('%.5f', 'acc'))
-    # Example Image
     from PIL import Image
     # Define the transformation: resize to 224x224 and convert to tensor
+
+    normalization = ((0.485, 0.456, 0.406),
+                     (0.229, 0.224, 0.225))
     transform = transforms.Compose([
-        # transforms.Resize((224, 224)),
-        transforms.ToTensor()
-    ])
+            transforms.Resize(size=int(resolution * 256/224)),
+            transforms.CenterCrop(size=resolution),
+            transforms.ToTensor(),
+            transforms.Normalize(normalization[0], normalization[1])])
     x = []
+
     # Define the path to the image
-    for i in range(10):
+    #  loop through every folder in
+    # /Users/akramreshad/nyu_grad_school/2024Spring/Deep Learning/final_project/dataset/unlabeled
+    # videos = []
+    # base_path = "/Users/akramreshad/nyu_grad_school/2024Spring/Deep Learning/final_project/dataset/unlabeled"
+    # for folder in sorted(os.listdir(base_path)):
+    #     folder_path = os.path.join(base_path, folder)
+    #     if "video" in folder_path:
+    #         print(folder)
+    #         for frame in os.listdir(folder_path):
+    #                 image_path = os.path.join(folder_path, frame)
+    #                 with Image.open(image_path) as image:
+    #                     videos.append(image.copy())  # Make a copy of the image if you need to store it
+
+    # print(len(videos))
+
+    for i in range(1):
         image_path = f"/Users/akramreshad/nyu_grad_school/2024Spring/Deep Learning/final_project/dataset/train/video_00000/image_{i}.png"
         
         # Load the image
@@ -157,9 +137,15 @@ def main(args_eval, resume_preempt=False):
         x.append(tensor_image)
 
     tensor_image = torch.stack(x)
+
+    # tensor_image.to("mps")
     print(tensor_image.shape)
-        
-    
+    # swap 2nd and 1st
+    # tensor_image = tensor_image.permute(1, 0, 2, 3)
+    print(tensor_image.shape)
+    # add batch dimension
+    # tensor_image = [[tensor_image.unsqueeze(0)]]
+    # print(tensor_image[0][0].shape)
     # Initialize model
     # -- pretrained encoder (frozen)
     encoder = init_model(
@@ -168,205 +154,29 @@ def main(args_eval, resume_preempt=False):
         pretrained=pretrained_path,
         model_name=model_name,
         patch_size=patch_size,
-        frames_per_clip=frames_per_clip,
+        frames_per_clip=pretrain_frames_per_clip,
         tubelet_size=tubelet_size,
         uniform_power=uniform_power,
         checkpoint_key=checkpoint_key,
         use_SiLU=use_SiLU,
         tight_SiLU=tight_SiLU,
         use_sdpa=use_sdpa)
+
+    
+    # Process each video clip independently and aggregate
+    # encoder = ClipAggregation(
+    #     encoder,
+    #     tubelet_size=tubelet_size,
+    #     attend_across_segments=attend_across_segments
+    # ).to(device)
     encoder.eval()
-    output = encoder(tensor_image)
-    print(output.shape)
-    exit()
     for p in encoder.parameters():
         p.requires_grad = False
 
 
-
-    # -- init classifier
-    classifier = AttentiveClassifier(
-        embed_dim=encoder.embed_dim,
-        num_heads=encoder.num_heads,
-        depth=1,
-        num_classes=num_classes
-    ).to(device)
-
-    train_loader = make_dataloader(
-        dataset_name=dataset_name,
-        root_path=root_path,
-        resolution=resolution,
-        image_folder=image_folder,
-        batch_size=batch_size,
-        world_size=world_size,
-        rank=rank,
-        training=True)
-    ipe = len(train_loader)
-
-
-    # # -- optimizer and scheduler
-    optimizer, scaler, scheduler, wd_scheduler = init_opt(
-        classifier=classifier,
-        wd=wd,
-        start_lr=start_lr,
-        ref_lr=lr,
-        final_lr=final_lr,
-        iterations_per_epoch=ipe,
-        warmup=warmup,
-        num_epochs=num_epochs,
-        use_bfloat16=use_bfloat16)
-    # classifier = DistributedDataParallel(classifier, static_graph=True)
-
-    # -- load training checkpoint
-    start_epoch = 0
-    if resume_checkpoint:
-        classifier, optimizer, scaler, start_epoch = load_checkpoint(
-            device=device,
-            r_path=latest_path,
-            classifier=classifier,
-            opt=optimizer,
-            scaler=scaler)
-        for _ in range(start_epoch*ipe):
-            scheduler.step()
-            wd_scheduler.step()
-
-    def save_checkpoint(epoch):
-        save_dict = {
-            'classifier': classifier.state_dict(),
-            'opt': optimizer.state_dict(),
-            'scaler': None if scaler is None else scaler.state_dict(),
-            'epoch': epoch,
-            'batch_size': batch_size,
-            'world_size': world_size,
-            'lr': lr
-        }
-        if rank == 0:
-            torch.save(save_dict, latest_path)
-
-    # TRAIN LOOP
-    for epoch in range(start_epoch, num_epochs):
-        logger.info('Epoch %d' % (epoch + 1))
-        output = run_one_epoch(
-            device=device,
-            training=True,
-            encoder=encoder,
-            classifier=classifier,
-            scaler=scaler,
-            optimizer=optimizer,
-            scheduler=scheduler,
-            wd_scheduler=wd_scheduler,
-            data_loader=train_loader,
-            use_bfloat16=use_bfloat16)
-
-        # val_acc = run_one_epoch(
-        #     device=device,
-        #     training=False,
-        #     encoder=encoder,
-        #     classifier=classifier,
-        #     scaler=scaler,
-        #     optimizer=optimizer,
-        #     scheduler=scheduler,
-        #     wd_scheduler=wd_scheduler,
-        #     data_loader=val_loader,
-        #     use_bfloat16=use_bfloat16)
-
-        # logger.info('[%5d] train: %.3f%% test: %.3f%%' % (epoch + 1, train_acc, val_acc))
-        # if rank == 0:
-        #     csv_logger.log(epoch + 1, train_acc, val_acc)
-        # save_checkpoint(epoch + 1)
-
-
-def run_one_epoch(
-    device,
-    training,
-    encoder,
-    classifier,
-    scaler,
-    optimizer,
-    scheduler,
-    wd_scheduler,
-    data_loader,
-    use_bfloat16,
-):
-
-    classifier.train(mode=training)
-    criterion = torch.nn.CrossEntropyLoss()
-    top1_meter = AverageMeter()
-
-    embeddings = []
-
-    for itr, data in enumerate(data_loader):
-
-        if training:
-            scheduler.step()
-            wd_scheduler.step()
-
-        with torch.cuda.amp.autocast(dtype=torch.float16, enabled=use_bfloat16):
-
-            imgs, labels = data[0].to(device), data[1].to(device)
-            with torch.no_grad():
-                outputs = encoder(imgs)
-                embeddings.append(outputs)
-                if not training:
-                    outputs = classifier(outputs)
-            if training:
-                outputs = classifier(outputs)
-
-        loss = criterion(outputs, labels)
-        top1_acc = 100. * outputs.max(dim=1).indices.eq(labels).sum() / len(imgs)
-        top1_acc = float(AllReduce.apply(top1_acc))
-        top1_meter.update(top1_acc)
-
-        if training:
-            if use_bfloat16:
-                scaler.scale(loss).backward()
-                scaler.unscale_(optimizer)
-                torch.nn.utils.clip_grad_norm_(classifier.parameters(), 1.0)
-                scaler.step(optimizer)
-                scaler.update()
-            else:
-                loss.backward()
-                torch.nn.utils.clip_grad_norm_(classifier.parameters(), 1.0)
-                optimizer.step()
-            optimizer.zero_grad()
-
-        if itr % 20 == 0:
-            logger.info('[%5d] %.3f%% (loss: %.3f) [mem: %.2e]'
-                        % (itr, top1_meter.avg, loss,
-                           torch.cuda.max_memory_allocated() / 1024.**2))
-
-    return embeddings
-
-
-def load_checkpoint(
-    device,
-    r_path,
-    classifier,
-    opt,
-    scaler
-):
-    try:
-        checkpoint = torch.load(r_path, map_location=torch.device('cpu'))
-        epoch = checkpoint['epoch']
-
-        # -- loading encoder
-        pretrained_dict = checkpoint['classifier']
-        msg = classifier.load_state_dict(pretrained_dict)
-        logger.info(f'loaded pretrained classifier from epoch {epoch} with msg: {msg}')
-
-        # -- loading optimizer
-        opt.load_state_dict(checkpoint['opt'])
-        if scaler is not None:
-            scaler.load_state_dict(checkpoint['scaler'])
-        logger.info(f'loaded optimizers from epoch {epoch}')
-        logger.info(f'read-path: {r_path}')
-        del checkpoint
-
-    except Exception as e:
-        logger.info(f'Encountered exception when loading checkpoint {e}')
-        epoch = 0
-
-    return classifier, opt, scaler, epoch
+    encoder.eval()
+    output = encoder(tensor_image)
+    print(output.shape)
 
 
 def load_pretrained(
@@ -396,53 +206,35 @@ def load_pretrained(
     del checkpoint
     return encoder
 
+# def init_model(
+#     device,
+#     pretrained,
+#     model_name,
+#     patch_size=16,
+#     crop_size=224,
+#     # Video specific parameters
+#     frames_per_clip=16,
+#     tubelet_size=2,
+#     use_sdpa=False,
+#     use_SiLU=False,
+#     tight_SiLU=True,
+#     uniform_power=False,
+#     checkpoint_key='target_encoder'
+# ):
+#     encoder = vit.__dict__[model_name](
+#         img_size=crop_size,
+#         patch_size=patch_size,
+#         num_frames=frames_per_clip,
+#         tubelet_size=tubelet_size,
+#         uniform_power=uniform_power,
+#         use_sdpa=use_sdpa,
+#         use_SiLU=use_SiLU,
+#         tight_SiLU=tight_SiLU,
+#     )
 
-def make_dataloader(
-    dataset_name,
-    root_path,
-    image_folder,
-    batch_size,
-    world_size,
-    rank,
-    resolution=224,
-    training=False,
-    subset_file=None
-):
-    normalization = ((0.485, 0.456, 0.406),
-                     (0.229, 0.224, 0.225))
-    if training:
-        logger.info('implementing auto-agument strategy')
-        transform = timm_make_transforms(
-            input_size=resolution,
-            is_training=training,
-            auto_augment='original',
-            interpolation='bicubic',
-            re_prob=0.25,
-            re_mode='pixel',
-            re_count=1,
-            mean=normalization[0],
-            std=normalization[1])
-    else:
-        transform = transforms.Compose([
-            transforms.Resize(size=int(resolution * 256/224)),
-            transforms.CenterCrop(size=resolution),
-            transforms.ToTensor(),
-            transforms.Normalize(normalization[0], normalization[1])])
-
-    data_loader, _ = init_data(
-        data=dataset_name,
-        transform=transform,
-        batch_size=batch_size,
-        world_size=world_size,
-        rank=rank,
-        root_path=root_path,
-        image_folder=image_folder,
-        training=training,
-        copy_data=False,
-        drop_last=False,
-        subset_file=subset_file)
-    return data_loader
-
+#     encoder.to(device)
+#     encoder = load_pretrained(encoder=encoder, pretrained=pretrained, checkpoint_key=checkpoint_key)
+#     return encoder
 
 def init_model(
     device,
@@ -480,45 +272,3 @@ def init_model(
     encoder.to(device)
     encoder = load_pretrained(encoder=encoder, pretrained=pretrained, checkpoint_key=checkpoint_key)
     return encoder
-
-
-def init_opt(
-    classifier,
-    iterations_per_epoch,
-    start_lr,
-    ref_lr,
-    warmup,
-    num_epochs,
-    wd=1e-6,
-    final_wd=1e-6,
-    final_lr=0.0,
-    use_bfloat16=False
-):
-    param_groups = [
-        {
-            'params': (p for n, p in classifier.named_parameters()
-                       if ('bias' not in n) and (len(p.shape) != 1))
-        }, {
-            'params': (p for n, p in classifier.named_parameters()
-                       if ('bias' in n) or (len(p.shape) == 1)),
-            'WD_exclude': True,
-            'weight_decay': 0
-        }
-    ]
-
-    logger.info('Using AdamW')
-    optimizer = torch.optim.AdamW(param_groups)
-    scheduler = WarmupCosineSchedule(
-        optimizer,
-        warmup_steps=int(warmup*iterations_per_epoch),
-        start_lr=start_lr,
-        ref_lr=ref_lr,
-        final_lr=final_lr,
-        T_max=int(num_epochs*iterations_per_epoch))
-    wd_scheduler = CosineWDSchedule(
-        optimizer,
-        ref_wd=wd,
-        final_wd=final_wd,
-        T_max=int(num_epochs*iterations_per_epoch))
-    scaler = torch.cuda.amp.GradScaler() if use_bfloat16 else None
-    return optimizer, scaler, scheduler, wd_scheduler
